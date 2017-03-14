fb103d7740eb5e7a6c2802ca62f3de72dd74b7a6
Balint Molnar <balint.molnar@organicawater.com>
2017-03-14 12:08:43 +0100

    adds math accessories (gcd)

--- main.cpp
+++ main.cpp
@@ -5,22 +5,22 @@
 #include "src/quantity.h"
 #include <ctime>
 #include <math.h>
-
+#include "lib/Accesories/math.hpp"
 
 using namespace std;
 using namespace munits;
-
+using namespace math;
 
 
 int main() {
 
 
-    Quantity mq (MassFlow, 1, "kg d-1");
-    Quantity r (_Last, 1, "kg m-2 d-1");
+//    Quantity mq (MassFlow, 1, "kg d-1");
+//    Quantity r (_Last, 1, "kg m-2 d-1");
+//
+//    auto res = mq /r;
 
-    auto res = mq /r;
 
-    cout << res << endl;
 
     try {
 //        for (int j = 0; j < 10; ++j) {
