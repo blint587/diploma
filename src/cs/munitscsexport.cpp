d706e61542997c40909c4c8582dfe843dcd391e3
Balint Molnar <balint.molnar@organicawater.com>
2018-08-16 12:18:26 +0200

    OCS-1247: adds arithmetic and comparison operator externs

--- src/cs/munitscsexport.cpp
+++ src/cs/munitscsexport.cpp
@@ -15,18 +15,91 @@ extern "C" {
     }
 
     // toString method
-    __declspec(dllexport) void toCSString(munits::Quantity * q,  char * buffer, int buffersize){
+    __declspec(dllexport) void __toString(munits::Quantity * q,  char * buffer, int buffersize){
         auto temp = (std::string) *q;
 
          std::copy(temp.begin(), temp.end(), buffer);
     }
-    __declspec(dllexport) double GetValueForCS(munits::Quantity * q){
+    __declspec(dllexport) double __getValue(munits::Quantity * q){
         return q->getValue();
     }
-    __declspec(dllexport) munits::Quantity* AddForCS(munits::Quantity * lft, munits::Quantity * rgh){
+    //add
+    __declspec(dllexport) munits::Quantity* __add(munits::Quantity * lft, munits::Quantity * rgh){
         munits::Quantity temp = *lft + *rgh;
         return new munits::Quantity((munits::metrics) temp.getMatrixIndex(), temp.getValue(), temp.getUnit());
     }
+    //substract
+    __declspec(dllexport) munits::Quantity* __substract(munits::Quantity * lft, munits::Quantity * rgh){
+        munits::Quantity temp = *lft - *rgh;
+        return new munits::Quantity((munits::metrics) temp.getMatrixIndex(), temp.getValue(), temp.getUnit());
+    }
+    //multiply
+    __declspec(dllexport) munits::Quantity* __multiply(munits::Quantity * lft, munits::Quantity * rgh){
+        munits::Quantity temp = *lft * *rgh;
+        return new munits::Quantity((munits::metrics) temp.getMatrixIndex(), temp.getValue(), temp.getUnit());
+    }
+    __declspec(dllexport) munits::Quantity* __multiply_rgh_int(munits::Quantity * lft, int rgh){
+        munits::Quantity temp = *lft * rgh;
+        return new munits::Quantity((munits::metrics) temp.getMatrixIndex(), temp.getValue(), temp.getUnit());
+    }
+   __declspec(dllexport) munits::Quantity* __multiply_lft_int(int lft, munits::Quantity * rgh){
+        munits::Quantity temp = lft * *rgh;
+        return new munits::Quantity((munits::metrics) temp.getMatrixIndex(), temp.getValue(), temp.getUnit());
+    }
+    __declspec(dllexport) munits::Quantity* __multiply_rgh_double(munits::Quantity * lft, double rgh){
+        munits::Quantity temp = *lft * rgh;
+        return new munits::Quantity((munits::metrics) temp.getMatrixIndex(), temp.getValue(), temp.getUnit());
+    }
+   __declspec(dllexport) munits::Quantity* __multiply_lft_double(double lft, munits::Quantity * rgh){
+        munits::Quantity temp = lft * *rgh;
+        return new munits::Quantity((munits::metrics) temp.getMatrixIndex(), temp.getValue(), temp.getUnit());
+    }
+    //divide
+   __declspec(dllexport) munits::Quantity* __divide(munits::Quantity * lft, munits::Quantity * rgh){
+        munits::Quantity temp = *lft / *rgh;
+        return new munits::Quantity((munits::metrics) temp.getMatrixIndex(), temp.getValue(), temp.getUnit());
+    }
+   __declspec(dllexport) munits::Quantity* __divide_int(munits::Quantity * lft, int rgh){
+        munits::Quantity temp = *lft / rgh;
+        return new munits::Quantity((munits::metrics) temp.getMatrixIndex(), temp.getValue(), temp.getUnit());
+    }
+
+   __declspec(dllexport) munits::Quantity* __divide_double(munits::Quantity * lft, double rgh){
+        munits::Quantity temp = *lft / rgh;
+        return new munits::Quantity((munits::metrics) temp.getMatrixIndex(), temp.getValue(), temp.getUnit());
+    }
+
+    //comparison
+    __declspec(dllexport) bool __lt(munits::Quantity * lft, munits::Quantity * rgh){
+        return *lft < *rgh;
+    }
+    __declspec(dllexport) bool __gt(munits::Quantity * lft, munits::Quantity * rgh){
+        return *lft > *rgh;
+    }
+    __declspec(dllexport) bool __le(munits::Quantity * lft, munits::Quantity * rgh){
+        return *lft <= *rgh;
+    }
+    __declspec(dllexport) bool __ge(munits::Quantity * lft, munits::Quantity * rgh){
+        return *lft >= *rgh;
+    }
+    __declspec(dllexport) bool __eq(munits::Quantity * lft, munits::Quantity * rgh){
+        return *lft == *rgh;
+    }
+    __declspec(dllexport) bool __ne(munits::Quantity * lft, munits::Quantity * rgh){
+        return *lft != *rgh;
+    }
+
+    //power
+    __declspec(dllexport) munits::Quantity * __pow(munits::Quantity * lft, int rgh){
+        munits::Quantity temp = munits::pow(*lft,  rgh);
+        return new munits::Quantity((munits::metrics) temp.getMatrixIndex(), temp.getValue(), temp.getUnit());
+    }
+    //nt roth
+    __declspec(dllexport) munits::Quantity * __ntrt(munits::Quantity * lft, int rgh){
+        munits::Quantity temp = lft->ntrt(rgh);
+        return new munits::Quantity((munits::metrics) temp.getMatrixIndex(), temp.getValue(), temp.getUnit());
+}
+
 
 
 }
