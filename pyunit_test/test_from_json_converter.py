aaa8273de5de86928c24b53b29aaa15c6c5accaf
Balint Molnar <blint587@gmail.com>
2022-08-22 15:49:50 +0200

    clean up - no json converter test

--- pyunit_test/test_from_json_converter.py
+++ pyunit_test/test_from_json_converter.py
@@ -1,26 +1,26 @@
-# encoding: utf-8
-import json
-from unittest import TestCase
-from munits import Length
-from base2.parsers import ToJsonParser, FromJsonParser
-from base2.base import BaseClass
-
-
-class Foo(BaseClass):
-
-    def __init__(self):
-        super().__init__()
-        self.something = Length(1, "m")
-
-
-class TestFromJSONParser(TestCase):
-
-    def test_from_json_parser_injection(self):
-        f = Foo()
-
-        js = json.dumps(f, default=ToJsonParser(with_class=True))
-        r = json.loads(js, object_hook=FromJsonParser())
-
-        self.assertIsInstance(r, Foo)
-        self.assertIsInstance(r.something, Length)
-
+# # encoding: utf-8
+# import json
+# from unittest import TestCase
+# from munits import Length
+# from base2.parsers import ToJsonParser, FromJsonParser
+# from base2.base import BaseClass
+#
+#
+# class Foo(BaseClass):
+#
+#     def __init__(self):
+#         super().__init__()
+#         self.something = Length(1, "m")
+#
+#
+# class TestFromJSONParser(TestCase):
+#
+#     def test_from_json_parser_injection(self):
+#         f = Foo()
+#
+#         js = json.dumps(f, default=ToJsonParser(with_class=True))
+#         r = json.loads(js, object_hook=FromJsonParser())
+#
+#         self.assertIsInstance(r, Foo)
+#         self.assertIsInstance(r.something, Length)
+#
